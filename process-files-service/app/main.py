ff56da5b00f1eedb97d240c3a601d3b7ed6d8cac
Moshe <moshe@example.com>
2026-03-01 23:12:34 +0200

    fix: main

--- process-files-service/app/main.py
+++ process-files-service/app/main.py
@@ -1,11 +1,32 @@
 import logging 
-from config import ServiceConfig
+from manager import ServiceManager
+from process_files import OpenFiles
+from service_config import ServiceConfig
+from shared.kafka.producer import KafkaProducer
 
 config = ServiceConfig()
 config.validate()
 
-logger = logging.basicConfig(
+logging.basicConfig(
     level=config.level_log,
-    format="",
-    
+    format=f'%(asctime)s | {config.service_name} | %(name)s - %(levelname)s -  %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+producer_logger = logging.getLogger("producer")
+open_files_logger = logging.getLogger("open_files")
+manager_logger = logging.getLogger("manager")
+
+def main():
+    try:
+        open_files = OpenFiles(config.data_path,open_files_logger)
+        producer = KafkaProducer(config.kafka_connect,config.topic,producer_logger)
+        manager = ServiceManager(producer,open_files,manager_logger)
+
+        manager.run()
+
+    except Exception:
+        raise
+
+if __name__=="__main__":
+    main()
