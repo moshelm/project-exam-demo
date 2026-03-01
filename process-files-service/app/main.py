87d76c0d656e7f2074aeabdaa5f5ec94feacf6b5
Moshe <moshe@example.com>
2026-03-01 23:58:35 +0200

    style: better names

--- process-files-service/app/main.py
+++ process-files-service/app/main.py
@@ -1,6 +1,6 @@
 import logging 
-from manager import ServiceManager
-from process_files import OpenFiles
+from manager import FileProcessor
+from process_files import FileMetadata
 from service_config import ServiceConfig
 from shared.kafka.producer import KafkaProducer
 
@@ -14,14 +14,14 @@ logging.basicConfig(
 )
 
 producer_logger = logging.getLogger("producer")
-open_files_logger = logging.getLogger("open_files")
+file_processor_logger = logging.getLogger("file_processor")
 manager_logger = logging.getLogger("manager")
 
 def main():
     try:
-        open_files = OpenFiles(config.data_path,open_files_logger)
+        file_processor = FileMetadata(config.data_path,file_processor_logger)
         producer = KafkaProducer(config.kafka_connect,config.topic,producer_logger)
-        manager = ServiceManager(producer,open_files,manager_logger)
+        manager = FileProcessor(producer,file_processor,manager_logger)
 
         manager.run()
 
