d18dc1fd297e8061f06a29b4739d3c18e16a55b2
Moshe <moshe@example.com>
2026-03-09 10:44:55 +0200

    feat: create analyze service files

---
+++ analyze-service/app/main.py
@@ -0,0 +1,44 @@
+import logging 
+from analyze_logic import Analyzer
+from service_config import ServiceConfig
+from shared.elasticsearch_connection import ElasticConnection
+from shared.logger_elastic import Logger
+from shared.kafka.consumer import KafkaConsumer
+
+config = ServiceConfig()
+config.validate()
+
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+logging.getLogger("elasticsearch").setLevel(logging.ERROR)
+logging.getLogger("confluent_kafka").setLevel(logging.ERROR)
+
+logger = Logger.get_logger(config.service_name, config.elastic_config, config.index_logger)
+
+# logging.basicConfig(
+#     level=config.level_log,
+#     format=f'%(asctime)s | {config.service_name} | %(name)s - %(levelname)s -  %(message)s',
+#     datefmt='%Y-%m-%d %H:%M:%S'
+# )
+
+# consumer_logger = logging.getLogger("consumer")
+# file_processor_logger = logging.getLogger("file_processor")
+# manager_logger = logging.getLogger("manager")
+# mongo_logger = logging.getLogger("mongodb")
+# elastic_logger = logging.getLogger("elastic")
+# mongo_tor_logger = logging.getLogger("stor gridfs")
+
+
+def main():
+    try:
+        consumer = KafkaConsumer(config.kafka_connect, config.topics_consumer, logger)
+        elastic = ElasticConnection(config.elastic_config, config.index_name, logger)
+        analyzer = Analyzer(config.hostile_list, config.less_hostile_list, consumer, elastic, logger)
+        analyzer.run()
+    except Exception:
+        raise
+
+if __name__=="__main__":
+    try:
+        main()
+    except Exception:
+        raise
